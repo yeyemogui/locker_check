6d407a45871d7a1ffcec68e0e8670fe0892a44cf
yeyemogui <sleep.flyer@hotmail.com>
2021-06-02 11:51:56 +0800

    Add logger support

--- Lock_Analyzer.py
+++ Lock_Analyzer.py
@@ -2,16 +2,22 @@
 import re, os
 import datetime
 import sys
+import optparse
+import logging
+import time
+
+global logger;
 
 class Lock_Analyzer:
     def __init__(self, logPath, threadPos = 8, timeStampPos = 7):
-        print("Will analyze log file (" + logPath + ").");
+        logger.info("Will analyze log file (" + logPath + ").");
         self.logPath = logPath;
         self.threadPos = threadPos;
         self.timeStampPos = timeStampPos;
         self.threadFilePosix = '_log.log';
         self.theadLockPosix = '_locks.log';
-        
+        self.threadFilePath = r'./ThreadFiles'
+        self.lockerFilePath = r'./LockerFiles'
         self.lockPairsMap = {
             'lock_shared': 'unlock_shared',
             'lock_upgrade': 'unlock_upgrade',
@@ -25,33 +31,35 @@ class Lock_Analyzer:
             'unlock_upgrade_and_lock': ['unlock_upgrade_and_lock(): Start unlock_upgrade_and_lock with addr', 'unlock_upgrade_and_lock(): unlock_upgrade_and_lock with addr'],
             'unlock_and_lock_upgrade': ['unlock_and_lock_upgrade(): Start unlock_and_lock_upgrade with addr', 'unlock_and_lock_upgrade(): unlock_and_lock_upgrade with addr']
             };
+        self.analyzeSum = [];
     
-    def closeFiles(self, FilesMap):
+    def closeThreadFiles(self, FilesMap):
         for key, value in FilesMap.items():
             if value is not None:
                 value.close();
 
     def __del__(self):
-        print("will clean resources");
+        logger.info("will clean resources");
 
-    def getFile(self, FilesMap, key, posix):
+    def getThreadFile(self, FilesMap, key, posix):
+        if not os.path.isdir(self.threadFilePath):
+            os.mkdir(self.threadFilePath);
         if key not in FilesMap.keys():
-            f = open(key + posix, 'w');
+            f = open(self.threadFilePath + r'/' + key + posix, 'w');
             FilesMap[key] = f;
         return FilesMap[key];
 
     def splitLogWithThreadId(self, threadFilesMap):
         log = open(self.logPath, 'r');
-        #threadFilesMap = {'empty': None};
         for line in log.readlines():
             if line.find('SharedMutexImpl.h') == -1:
                 continue;
             threadId = line.split(' ')[self.threadPos - 1];
             if threadId is None:
                 continue;
-            file = self.getFile(threadFilesMap, threadId, self.threadFilePosix);
-            file.write(line + "\n");
-        self.closeFiles(threadFilesMap);
+            file = self.getThreadFile(threadFilesMap, threadId, self.threadFilePosix);
+            file.write(line);
+        self.closeThreadFiles(threadFilesMap);
         log.close();
 
     def getLockInfo(self, line):
@@ -65,6 +73,8 @@ class Lock_Analyzer:
                     return lockId, key, timeStamp;
             return None, None, None;
         except Exception as ex:
+            logger.warning(str(ex));
+            logger.warning("error happened when handling: " + line.strip('\n'));
             return None, None, None
 
 
@@ -93,6 +103,9 @@ class Lock_Analyzer:
                     'lock_upgrade': [],
                     'unlock_upgrade_and_lock':[] };
             if lockType in lockState[lockId].keys():
+                if lockState[lockId][lockType] != 0:
+                    logger.warning("Detected recursive lock for thread: " + threadId + " with lock id: " + lockId + " with lock type: " + lockType + " at " + timeStamp)
+                    self.analyzeSum.append(['Recursive Locker', threadId, lockId, lockType, timeStamp]);
                 lockState[lockId][lockType] += 1;
                 record = [timeStamp, None, None];
                 tmpLockResult[lockId][lockType].append(record);
@@ -114,27 +127,38 @@ class Lock_Analyzer:
             for lockType in lockState[lockId].keys():
                 if lockState[lockId][lockType] != 0:
                     lockResult[lockId][lockType] += tmpLockResult[lockId][lockType];
+                    
+        self.storeLockDetails(threadId, lockResult);
+        log.close();
 
-        f = open(threadId + self.theadLockPosix, 'w');
+    def storeLockDetails(self, threadId, lockResult):
+        if not os.path.isdir(self.lockerFilePath):
+            os.mkdir(self.lockerFilePath);
+        f = open(self.lockerFilePath + r'/' + threadId + self.theadLockPosix, 'w');
         for key in lockResult.keys():
             f.write("Lock Id: " + key + "\n");
             for lockType, records in lockResult[key].items():
                 f.write("Lock Type: " + lockType + "\n");
                 for record in records:
                     if record[1] is None: 
-                        print("there is possible deadlock for thread " + threadId + " with lock id " + key + " with type " + lockType + " at " + record[0]);
+                        logger.error("there is possible deadlock for thread " + threadId + " with lock id " + key + " with type " + lockType + " at " + record[0]);
+                        self.analyzeSum.append(['No released locker', threadId, key, lockType, record[0]]);
                         f.write(record[0] + " " + " " + " " + " " + "\n");
                     else:
                         f.write(record[0] + " " + str(record[1]) + " " + str(record[2]) + "\n");
-
         f.close();
-        log.close();
 
     def calculateLockTime(self):
         threadFilesMap = {};
         self.splitLogWithThreadId(threadFilesMap);
         for threadId in threadFilesMap.keys():
-            self.analyzeThreadFile(threadId+self.threadFilePosix, threadId);
+            self.analyzeThreadFile(self.threadFilePath + r'/' + threadId+self.threadFilePosix, threadId);
+        logger.info("-------------Summary----------------")
+        for result in self.analyzeSum:
+            printInfo = "";
+            for item in result:
+                printInfo += item + " ";
+            logger.info(printInfo);
 
     def getTime(self, timeStamp):
         return datetime.datetime.strptime(timeStamp.strip('Zz'), "%Y-%m-%dT%H:%M:%S.%f");
@@ -145,8 +169,58 @@ class Lock_Analyzer:
             return None;
         return timeStamp.strip('<>');
 
+class ToolKit:  
+    @staticmethod
+    def getFileContent(fileName):
+        if fileName is None:
+            return None
+        content = []
+        f = open(fileName, 'r')
+        print("The content within " + fileName + " is:")
+        for line in f.readlines():
+            print(line.strip("\n"))
+            content.append(line.strip("\n"))
+        f.close()
+        return content
+
+    @staticmethod
+    def initParser():
+        parser = optparse.OptionParser();
+        parser.description = "This tool is used to detect:\n \
+            1) possible deadlock\n \
+            2) recursive lock\n \
+            3) the hold time of lock\n \
+            Usage:\n \
+                Lock_Analyzer.py -f log_path";
+        parser.add_option("-f", "--filePath", action = "store", dest = "logPath", help = "the path of log");
+        return parser;
+    
+    @staticmethod
+    def checkOption(options):
+        if options.logPath is None:
+            raise Exception("please specify the path of log");
+
+    @staticmethod
+    def initLogger():
+        LOG_FORMAT = '%(levelname)s - %(asctime)s - %(filename)s - %(lineno)d - %(message)s';
+        time_line = time.strftime('%Y%m%d%H%M', time.localtime(time.time()))
+        global logger;
+        logger = logging.getLogger();
+        logger.setLevel(logging.DEBUG);
+        logfile = r'./log_' + time_line + r'.txt';
+        fileHandler = logging.FileHandler(logfile);
+        fileHandler.setLevel(logging.DEBUG);
+        fileHandler.setFormatter(logging.Formatter(LOG_FORMAT));
+        logger.addHandler(fileHandler);
+        return logger;
+
 if __name__ == "__main__":
-    analyzer = Lock_Analyzer(sys.argv[1]);
+    parser = ToolKit.initParser();
+    (options, args) = parser.parse_args();
+    ToolKit.checkOption(options);
+    ToolKit.initLogger();
+
+    analyzer = Lock_Analyzer(options.logPath);
     analyzer.calculateLockTime();
 
      
